04d7027bd79f127e265e3f60926ed6c12e6958c6
Dhmes <dhmesonaraujo123@gmail.com.br>
2021-02-18 12:22:58 -0300

    Player movement done. Handle Player Input.

--- scripts/main.js
+++ scripts/main.js
@@ -6,21 +6,43 @@ class Bird{
     constructor(size){
         this.pos = new Vector2d(50,Math.ceil(canvas.height/2+size));
         this.size = size;
+        this.vel = 0;
     }
     draw(){
+        ctx.fillStyle = "#000000";
         ctx.fillRect(this.pos.x, this.pos.y, this.size, this.size);
+    }
+    move(){
+        console.log(minmax(-15,15,this.vel));
+        this.vel = minmax(-15,15,this.vel);
+        this.pos.y -= this.vel;
+        this.vel += -1;
     }
 }
 
 const player = new Bird(20);
 
+
 Start();
 Update();
 
 function Start(){
-    
+    canvas.focus();
+    canvas.addEventListener('click', HandleKeyPress);
 }
 
 function Update(){
+    ClearScreen();
+    player.move();
     player.draw();
+    setTimeout(Update, 30);
+}
+
+function ClearScreen(){
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function HandleKeyPress(e){
+    player.vel = 15;
 }
--- scripts/util.js
+++ scripts/util.js
@@ -4,3 +4,9 @@ class Vector2d{
         this.y = y;
     }
 }
+
+function minmax(min,max,value){
+    if(value <= min) return min;
+    if(value >= max) return max;
+    return value; 
+}
